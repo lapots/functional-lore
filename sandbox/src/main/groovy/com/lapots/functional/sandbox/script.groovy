4073d4cf0c96616632258f90fffbee6e2970d661
lapots <sebastrident@gmail.com>
2016-12-15 16:28:54 +0300

    Sandbox for groovy with unpacking XZ example

--- sandbox/src/main/groovy/com/lapots/functional/sandbox/script.groovy
+++ sandbox/src/main/groovy/com/lapots/functional/sandbox/script.groovy
@@ -1,3 +1,19 @@
 package com.lapots.functional.sandbox
 
-println "Hello, world!"
+import org.apache.commons.io.*;
+import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
+
+int buffersize = 1000
+
+FileInputStream fileStream = new FileInputStream("play\\ghc-8.0.1-x86_64-unknown-mingw32.tar.xz")
+BufferedInputStream input = new BufferedInputStream(fileStream)
+FileOutputStream out = new FileOutputStream("play\\ghc-8.0.1-x86_64-unknown-mingw32.tar")
+XZCompressorInputStream xzIn = new XZCompressorInputStream(input)
+
+final byte[] buffer = new byte[buffersize];
+int n = 0;
+while (-1 != (n = xzIn.read(buffer))) {
+    out.write(buffer, 0, n);
+}
+out.close();
+xzIn.close();
