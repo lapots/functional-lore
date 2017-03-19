f963a7221b7bf8bc071a1fafcb9c1b01a2a54fac
lapots <sebastrident@gmail.com>
2017-03-19 16:06:40 +0300

    CLJfFn p22-23

--- clojure-project/src/main/clojure/com/lapots/functional/clojure/edgar/core.clj
+++ clojure-project/src/main/clojure/com/lapots/functional/clojure/edgar/core.clj
@@ -1,9 +1,19 @@
 (ns com.lapots.functional.clojure.edgar.core
-    (:gen-class))
+    (:gen-class)
+    (:require [clj-time.core :as tc]
+              [clj-time.periodic :as tp]
+              [clj-time.coerce :as tco]))
 
 (defn generate-prices [lower-bound upper-bound]
     (filter (fn [x] (>= x lower-bound))
         (repeatedly (fn [] (rand upper-bound)))))
 
+(defn generate-timeseries [pricelist]
+    (map (fn [x y]
+            {:time x :price y})
+          (iterate inc 0)
+         pricelist))
+
 (defn -main [& args]
-    (println (take 10 (generate-prices 12 35))))
+    (def pricelist (generate-prices 12 35))
+    (println (take 10 (generate-timeseries pricelist))))
