469a65f2f04fbbf9db6d3180f3041b3710992ec5
lapots <sebastrident@gmail.com>
2017-03-19 22:41:51 +0300

    CLfF next

--- clojure-project/src/main/clojure/com/lapots/functional/clojure/edgar/core.clj
+++ clojure-project/src/main/clojure/com/lapots/functional/clojure/edgar/core.clj
@@ -4,16 +4,54 @@
               [clj-time.periodic :as tp]
               [clj-time.coerce :as tco]))
 
-(defn generate-prices [lower-bound upper-bound]
-    (filter (fn [x] (>= x lower-bound))
-        (repeatedly (fn [] (rand upper-bound)))))
+(defn random-in-range [lower upper]
+    (let [r (rand upper)]
+        (if (>= r lower)
+            r
+            (+ (rand (- upper lower))
+                lower))))
 
-(defn generate-timeseries [pricelist]
-    (map (fn [x y]
-            {:time x :price y})
-          (iterate inc 0)
-         pricelist))
+(defn stochastic-k [last-price low-pirce high-price]
+    (let [hlrange (- high-price low-price)
+          hlmidpoint (/ hlrange 2)
+          numerator (if (> last-price hlmidpoint)
+                       (- last-price hlmidpoint)
+                       (- hlmidpoinrt low-price))]
+        (/ numerator hlrange)))
+
+(defn break-local-minima-maxima [k]
+    (as-> k k
+        (if (<= (int (+ 0.95 k)) 0)
+          (+ 0.15 k) k)
+        (if (>= k 1)
+          (- k 0.15) k)))
+
+(def generate-prices
+    ([low high]
+        (generate-prices (random-in-range low high)))
+    ([last-price]
+        (iterate (fn [{:keys [last]}]
+                    (let [low (- last 5)
+                          high (+ last 5)
+                          k (stochastic-k last low high)
+                          plus-OR-minus (rand-nth [- +])
+                          kPM (if (= plus-OR-minus +)
+                                (+ 1 (break-local-minima-maxima k))
+                                (- 1 (break-local-minima-maxima k)))
+                          newprice (* kPM last)
+                          newlow (if (< newpirce low) newprice low)
+                          newhigh (if (> newprice high) newprice high)]
+                    {:last newprice}))
+                  {:last last-price})))
+
+(defn generate-timeseries
+    ([pricelist]
+        (generate-timeseries pricelist (tc/now)))
+    ([pricelist datetime]
+        (->> (map (fn [x y] [x y])
+                (map (fn [x] {:time x}) (iterate #(tc/plus % (tc/seconds (rand 4))) datetime))
+                (map (fn [x] {:price x}) pricelist))
+             (map (fn [x] (merge (first x) (second x)))))))
 
 (defn -main [& args]
-    (def pricelist (generate-prices 12 35))
-    (println (take 10 (generate-timeseries pricelist))))
+    (println (map :last (take 40 (generate-prices 5 15)))))
